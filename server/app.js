d1ab2beff6b01a8700048d399e4c4378635c2a61
emmanuel <emmanuel.devpro@gmail.com>
2026-03-06 01:53:06 +0100

    Server side: Added config/db.js for database connection

--- server/app.js
+++ server/app.js
@@ -5,8 +5,24 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! This is home page");
 });
+
+app.post("/", (req, res) => {
+  res.send("Got a POST request");
+});
+
+app.put("/user", (req, res) => {
+  res.send("Got a PUT request at /user");
+});
+
+app.get("/users/:userId/books/:bookId", (req, res) => {
+  res.send(req.params);
+});
+
+// app.get("/user", (req, res) => {
+//   res.send("Got a GET request at /user");
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
