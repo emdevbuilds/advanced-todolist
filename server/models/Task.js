9ba3448958a655e3c4e489ca88ea58961cc70bbc
emmanuel <emmanuel.devpro@gmail.com>
2026-03-08 16:32:18 +0100

    Server side: define schema for the Task

---
+++ server/models/Task.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please add a title for the task"],
+    trim: true,
+    minLength: [3, "Title must be at least 3 characters."],
+    maxLength: [50, "Title must be at most 50 characters."],
+  },
+  description: {
+    type: String,
+    maxLength: [200, "Description must be at most 200 characters."],
+  },
+});
