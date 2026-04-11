46b00edea691baec67e0213559f86cd333178d7e
emmanuel <emmanuel.devpro@gmail.com>
2026-04-11 23:30:22 +0100

    ref(server): refactor app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 // BETTER AUTH ROUTES
-app.all("/api/auth/*", toNodeHandler(auth));
+app.all("/api/auth/{*path}", toNodeHandler(auth));
 
 app.use(express.json());
 
