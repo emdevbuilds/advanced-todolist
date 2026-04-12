6aa62851587eff50e1df55804e923af1f6ba8daf
emmanuel <emmanuel.devpro@gmail.com>
2026-04-12 14:57:26 +0100

    ref(server): Scope every query to the logged-in user in the taskController.js

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -2,8 +2,11 @@ import Task from "../models/Task.js";
 
 export const getTasks = async (req, res, next) => {
   try {
-    const tasks = await Task.find().sort({ createdAt: -1 });
+    const tasks = await Task.find({ userId: req.user.id }).sort({
+      createdAt: -1,
+    });
     res.json({ success: true, data: tasks });
+    // console.log(req.user);
   } catch (err) {
     next(err);
   }
@@ -13,7 +16,7 @@ export const createTask = async (req, res, next) => {
   try {
     const { title, description } = req.body;
 
-    const task = await Task.create({ title, description });
+    const task = await Task.create({ title, description, userId: req.user.id });
     res.status(201).json({ success: true, data: task });
   } catch (err) {
     next(err);
@@ -22,7 +25,17 @@ export const createTask = async (req, res, next) => {
 
 export const deleteTask = async (req, res, next) => {
   try {
-    await Task.findByIdAndDelete(req.params.id);
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      userId: req.user.id,
+    });
+
+    if (!task) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Task not found" });
+    }
+
     res.json({ success: true, message: "Task deleted successfully" });
   } catch (err) {
     next(err);
@@ -32,11 +45,10 @@ export const deleteTask = async (req, res, next) => {
 export const markTaskAsDone = async (req, res, next) => {
   try {
     const { completed } = req.body;
-    const task = await Task.findByIdAndUpdate(
-      req.params.id,
-      {
-        completed: completed,
-      },
+
+    const task = await Task.findOneAndUpdate(
+      { _id: req.params.id, userId: req.user.id },
+      { completed },
       { new: true, runValidators: true },
     );
 
